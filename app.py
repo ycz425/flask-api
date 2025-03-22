a9c2713e219cc480adf3d43d613b77bdb8628523
John Zhang <23zhanyuc@gmail.com>
2025-03-22 07:33:37 -0400

    Add file upload endpoint and text extraction utils

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from chatbot import vectorize_and_store
 
 app = Flask(__name__)
 
+FILE_TYPES = [
+    "application/pdf",  # PDF
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",  # PPTX
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",  # DOCX
+]
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def root():
+    return jsonify({
+        "message": "Welcome to the Course Dash API."
+    })
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    """Handles user file uploads."""
+    file = request.files.get("file")
+
+    if not file:
+        return jsonify({"error": "No file provided"}), 400
+
+    if file.content_type not in FILE_TYPES:
+        return jsonify({"error": "Unsupported file type"}), 400
+
+    # Process the file and store in ChromaDB (for chatbot)
+    vectorize_and_store(file)
+
+    # TODO: insert to database
+
+
+    return jsonify({"message": "File uploaded & processed successfully."})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ text_extraction.py
@@ -0,0 +1,40 @@
+from PyPDF2 import PdfReader
+from pptx import Presentation
+from docx import Document
+
+
+def extract_text(file):
+    if file.content_type == "application/pdf":
+        return extract_text_from_pdf(file)
+    elif file.content_type == "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+        return extract_text_from_pptx(file)
+    elif file.content_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        return extract_text_from_docx(file)
+    
+    return ""
+        
+
+def extract_text_from_pdf(file):
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+
+def extract_text_from_pptx(file):
+    prs = Presentation(file)
+    text = ""
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                text += shape.text
+    return text
+
+
+def extract_text_from_docx(file):
+    doc = Document(file)
+    text = ""
+    for para in doc.paragraphs:
+        text += para.text
+    return text
